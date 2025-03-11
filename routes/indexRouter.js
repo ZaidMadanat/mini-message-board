3facc17dde11e0ac34cde496573c49be556b0842
unknown <zaidsmadanat@gmail.com>
2025-03-11 16:54:58 -0700

    added pointer to form, getter function to retreive

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,7 @@ const { Router } = require("express");
 const indexRouter = Router(); 
 
 
-
+// Starting messages "Data Base"
 const messages = [ 
     { 
         text: "Hi there!", 
@@ -18,7 +18,17 @@ const messages = [
     }
 ];
 
-
+// Rendering EJS files for index and form, with 
+// the home route rendering a title and messages and new pointing to form
 indexRouter.get("/", (req, res) => res.render("index", {title: "Mini Messageboard", messages: messages})); 
+indexRouter.get("/new", (req, res) => res.render("form"))
+
+// Taking the request from the form and pushing it onto our messages. 
+indexRouter.post("/new", (req, res) => { 
+    messageText = req.body.messageText
+    user = req.body.user; 
+    messages.push({text: messageText, user: user, added: new Date()});
+    res.redirect("/");
+});
 
 module.exports = indexRouter;
